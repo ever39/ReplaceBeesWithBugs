e8140620c35a86f20f78f7a324c3cf0265110dc2
ever39 but in visual studio <command9874@protonmail.com>
2024-05-16 20:22:13 +0300

    added multiple config options, minimum and maximum bug spawn amounts, random range of time that bugs can spawn at

--- ReplaceBeesWithBugs/Plugin.cs
+++ ReplaceBeesWithBugs/Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
@@ -9,10 +10,19 @@ namespace ReplaceBeesWithBugs
     public class Plugin : BaseUnityPlugin
     {
         internal static ManualLogSource mls;
+        public static ConfigEntry<bool> killBeesOnRoundStart;
+        public static ConfigEntry<bool> spawnBugsOvertime;
+        public static ConfigEntry<int> minBugAmount;
+        public static ConfigEntry<int> maxBugAmount;
 
         private void Awake()
         {
             mls = Logger;
+            killBeesOnRoundStart = Config.Bind("General", "Kill bees on round start?", true, "Also spawns initial hoarding bugs!");
+            spawnBugsOvertime = Config.Bind("General", "Spawn bugs over time", true, "Spawns bugs overtime based on a random interval between 30 and 120 seconds");
+            minBugAmount = Config.Bind("General", "Minimum bug amount per spawn cycle", 1);
+            maxBugAmount = Config.Bind("General", "Maximum bug amount per spawn cycle", 5);
+
             new Harmony("ReplaceBeesWithHoardingBugs").PatchAll(typeof(StartOfRoundPatches));
             mls.LogInfo("patch applied, bees will be replaced with hoarding bugs");
         }
@@ -20,13 +30,21 @@ namespace ReplaceBeesWithBugs
 
     public class StartOfRoundPatches
     {
+        static bool isHost = false;
         static EnemyType hoardingBugEnemyType;
         static int beeHiveAmount = 0;
+        static float bugSpawnCooldown;
+        static int maxBugs;
+        static int minBugs;
         [HarmonyPatch(typeof(RoundManager), nameof(RoundManager.LoadNewLevel))]
         [HarmonyPostfix]
         static void GetHoardingBugEnemyType(ref SelectableLevel newLevel)
         {
-            if (!GameNetworkManager.Instance.isHostingGame)
+            isHost = GameNetworkManager.Instance.isHostingGame;
+            maxBugs = Plugin.maxBugAmount.Value;
+            minBugs = Plugin.minBugAmount.Value;
+
+            if (!isHost)
             {
                 return;
             }
@@ -46,29 +64,74 @@ namespace ReplaceBeesWithBugs
         [HarmonyPostfix]
         static void ReplaceBeesWithHoardingBugs()
         {
-            if (!GameNetworkManager.Instance.isHostingGame)
+            if (!isHost)
             {
                 Plugin.mls.LogWarning("NOT HOST, returning");
+                return;
+            }
+
+            bugSpawnCooldown = UnityEngine.Random.Range(20, 120);
+
+            if (!Plugin.killBeesOnRoundStart.Value)
+            {
+                Plugin.mls.LogInfo("Not killing bees, not spawning initial bugs");
                 return;
             }
 
             try
             {
                 RedLocustBees[] bees = Object.FindObjectsOfType<RedLocustBees>();
-                foreach (RedLocustBees bee in bees)
+                if (bees != null)
                 {
-                    Vector3 beePos = bee.transform.position;
-                    ((EnemyAI)bee).enemyType.canDie = true;
-                    ((EnemyAI)bee).KillEnemyClientRpc(true);
-                    ((EnemyAI)bee).KillEnemyOnOwnerClient(true);
-                    RoundManager.Instance.SpawnEnemyGameObject(beePos, 0, 1, hoardingBugEnemyType);
-                    beeHiveAmount++;
-                    Plugin.mls.LogInfo($"Spawned hoarding bug to replace dead bees at position {beePos} ({beeHiveAmount})");
+                    foreach (RedLocustBees bee in bees)
+                    {
+                        Vector3 beePos = bee.transform.position;
+                        ((EnemyAI)bee).enemyType.canDie = true;
+                        ((EnemyAI)bee).KillEnemyClientRpc(true);
+                        ((EnemyAI)bee).KillEnemyOnOwnerClient(true);
+
+                        RoundManager.Instance.SpawnEnemyGameObject(beePos, 0, UnityEngine.Random.Range(minBugs, maxBugs), hoardingBugEnemyType);
+                        beeHiveAmount++;
+                        Plugin.mls.LogInfo($"Spawned initial hoarding bug to replace dead bees at position {beePos} ({beeHiveAmount})");
+                    }
                 }
             }
             catch
             {
                 return;
+            }
+        }
+
+        [HarmonyPatch(typeof(RoundManager), "Update")]
+        [HarmonyPostfix]
+        static void RandomlySpawnBugsFromHives()
+        {
+            if (isHost)
+            {
+                bugSpawnCooldown -= Time.deltaTime;
+
+                if (bugSpawnCooldown < 0 && Plugin.spawnBugsOvertime.Value)
+                {
+                    GameObject[] hives = GameObject.FindGameObjectsWithTag("PhysicsProp");
+
+                    if (hives.Length == 0 | hives == null)
+                    {
+                        Plugin.mls.LogInfo("NO HIVES, returning");
+                        bugSpawnCooldown = UnityEngine.Random.Range(20, 120);
+                        return;
+                    }
+
+                    foreach (GameObject hive in hives)
+                    {
+                        if (hive.name == "RedLocustHive(Clone)" && !hive.GetComponent<PhysicsProp>().isInShipRoom)
+                        {
+                            Vector3 hivePos = hive.transform.position;
+                            RoundManager.Instance.SpawnEnemyGameObject(hivePos, 0, UnityEngine.Random.Range(minBugs, maxBugs), hoardingBugEnemyType);
+                        }
+                    }
+
+                    bugSpawnCooldown = UnityEngine.Random.Range(20, 120);
+                }
             }
         }
     }
